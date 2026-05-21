6cbca88d36ee16e8d46feb51d5e8f56c4ffba87f
Subodh2312 <159807214+SubodhM214@users.noreply.github.com>
2026-05-21 15:53:30 +0530

    intial commit containing pages and zustand

---
+++ client/src/store/authStore.js
@@ -0,0 +1,21 @@
+import { create } from "zustand";
+
+const useAuthStore = create((set) => ({
+  user: null,
+  token: null,
+  setAuth: (user, token) => {
+    localStorage.setItem("token", token);
+    set({ user, token });
+  },
+  logout: () => {
+    localStorage.removeItem("token");
+    set({ user: null, token: null });
+  },
+  loadFromStorage: () => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      set({ user: { name: "Dummy Name" }, token });
+    }
+  },
+}));
+export default useAuthStore;
