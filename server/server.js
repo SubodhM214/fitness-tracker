3700262760bcf9922ad1e779aed1c1226a0b8a26
Subodh2312 <159807214+SubodhM214@users.noreply.github.com>
2026-06-08 14:49:34 +0530

    add frontend url in server.js

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173", // local dev
-      "https://fittrack.vercel.app", // production (update after Vercel deploy)
+      process.env.ALLOWED_ORIGIN, // production (update after Vercel deploy)
       /\.vercel\.app$/, // any vercel preview URL
     ],
     credentials: true,
